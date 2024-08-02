feda1786dd10ccbf2035e3a2a44354987a4c8eb0
isiagi <isiagigeofrey0@gmail.com>
2024-08-02 14:22:37 +0300

    chore: Add page not found

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <div className="h-screen flex items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold">NewsScrolls 404</h1>
+      <h1 className="pt-5">404 | Page not found</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
